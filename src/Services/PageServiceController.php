a01c5f0f1d10992616825142424ebec5cde64663
sogore <massire.org@gmail.com>
2022-09-02 20:55:45 +0200

    ajout de params global yaml and twig timezone

--- src/Services/PageServiceController.php
+++ src/Services/PageServiceController.php
@@ -4,9 +4,16 @@ namespace App\Services;
 
 use DateTimeImmutable;
 use DateTimeZone;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class PageServiceController
 {
+    // public $timezone;
+
+    // public function __construct(ParameterBagInterface $parameterBagInterface)
+    // {
+    //     dd($parameterBagInterface->get());
+    // }
     public function dayLeftUntilNextYear()
     {
         return (new DateTimeImmutable(' 1st January next Year ', (new DateTimeZone('Europe/Paris'))))->diff($this->dateNow());
